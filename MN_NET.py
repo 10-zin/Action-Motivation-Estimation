ebd6dd20d87cf227c2f83c38b9ce45a7b1821c9b
10-zin <tenzinbhotia0@gmail.com>
2020-01-26 09:36:26 +0530

    Script Update

---
+++ MN_NET.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+import numpy as np 
+import os
+import torch.autograd
+import torch.optim as optim
+import torch.nn.functional as F 
+from torchvision import models, transforms
+
+from torch.nn.utils.rnn import pack_padded_sequence 
+#import nltk
+#from nltk.corpus import stopwords
+#from nltk import tokenize
+
+MODEL_PATH = ''
+
+
+
+class MN_NET(torch.nn.Module):
+    def __init__(self, batch_size=1, seq_len=1,hidden_size = 2,embed_size = 1,vocab_size = 1, action_categories=0):
+        super(MN_NET, self).__init__()
+        self.batch_size = batch_size
+        self.hidden_size = hidden_size
+
+        self.embed_size = embed_size
+        self,vocab_size = vocab_size
+        #self.num_layers = num_layers
+        self.categories = action_categories
+        
+        self.max_seq_len = seq_len
+        self.resnet = models.resnet101(pretrained = True)
+        modules=list(self.resnet.children())[:-2]
+        self.resnet=nn.Sequential(*modules)
+        for p in self.resnet.parameters():
+            p.requires_grad = False
+       
+        self.linear_layer = nn.Linear(in_features = 2048*7*7, out_features = 2048)
+        self.dropout = nn.Dropout(0.4)
+        self.linear_layer_embed = nn.Linear(in_features = 2048,out_features = self.embed_size)
+        self.linear_classifier = nn.Linear(in_features = self.hidden_size, out_features= self.categories)
+
+        self.embed_layer = nn.Embedding(self.vocab_size,self.embed_size)
+        self.gru = nn.GRU(input_size = self.embed_size ,hidden_size = self.hidden_size,num_layers = 1,batch_first = True)
+        self.bn = nn.BatchNorm1d(self.embed_size, momentum = 0.01)
+        self.linear_to_vocab = nn.Linear(in_features=self.hidden_size,out_features= self.vocab_size)
+        
+        
+        
+        
+        
+    def forward(self, img, captions,lengths):
+        self.h_init = torch.rand((1,batch_size,self.hidden_size))
+        
+        features = self.get_embed_features(img)
+        
+        embeddings = self.embed_layer(captions)
+        embeddings = torch.cat((features.unsqueeze(1),embeddings), 1)
+
+       	packed = pack_padded_sequence(embeddings, lengths, batch_first=True) 
+        outputs, _ = self.gru(packed,self.h_init)
+
+       	"""h_out = h_out.transpose(0,1)
+        print(h_out.size())
+       	h_out = h_out.view(-1,self.num_layers*self.hidden_size)
+        print(h_out.size())
+       	h_out = F.softmax(self.linear_classifier(h_out))
+        """
+        outputs_fin = self.linear_to_vocab(outputs[0])
+
+       	return outputs_fin
+     #Sampling Function for Generating Caption
+    def sample(self, features, states= self.h_init):
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        # Looping for running GRU max_seq_len times.
+        for i in range(self.max_seq_len):
+            hiddens, states = self.gru(inputs, states)          
+            outputs = self.linear_to_vocab(hiddens.squeeze(1))            
+            _, predicted = outputs.max(1)                       
+            sampled_ids.append(predicted)
+            inputs = self.embed_layer(predicted)                      
+            inputs = inputs.unsqueeze(1)                         
+        
+        sampled_ids = torch.stack(sampled_ids, 1)                
+        
+        return sampled_ids
+    
+    def get_embed_features(self,img):
+        img = self.resnet(img)
+        img = img.view(-1,2048*7*7)
+
+       	img = F.relu(self.linear_layer(img))
+       	img = self.dropout(img)
+       	img = self.linear_layer_embed(img)
+        features = self.bn(img)
+        
+        return features
+    
+
+
+
+
+def train(model,data_loader,epochs = 10,checkpoint = 10):
+
+  model.train()
+  loss_criterion = nn.CrossEntropyLoss()
+  
+  optimizer = optim.Adam(model.parameters(),lr = 0.0002)
+  total_step = len(data_loader)
+
+  for epoch in range(epochs):
+
+    for i,(images,captions,lengths) in enumerate(data_loader):
+
+       targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
+       outputs = model(images,captions,lengths)
+       loss = loss_criterion(outputs,targets)
+
+       model.zero_grad()
+       loss.backward()
+       optimizer.step()
+
+       if i%5==0:
+        print('Epoch--->[{}/{}], Step--->[{}/{}], Loss--->{}'.format(epoch+1,epochs,i,total_step,loss.item()))
+
+    if (epoch+1)%checkpoint==0:
+        state = {
+        'epoch':epoch,
+        'state_dict':model.state_dict(),
+        'optimizer':optimizer.state_dict()
+        }
+        torch.save(state,MODEL_PATH)
+  
+
+def preprocess_img(image):
+    normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225]
+    )
+    preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize
+            ])
+    return preprocess(image)
+
+
+
+
+
+        		
+
+
+
+
+
+
---
+++ loader.py
@@ -0,0 +1,120 @@
+#matplotlib inline
+import numpy as np
+import torch
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+pylab.rcParams['figure.figsize'] = (8.0, 10.0)
+import sys
+#import sys.path
+sys.path.append(r'C:\Users\pasan\Documents\Motivation\cocoapi-master\PythonAPI')
+
+import pycocotools
+from pycocotools.coco import COCO
+import preprocess
+from PIL import Image
+
+class L:
+    def __init__(self):
+        self.dataDir='..'
+        self.dataType='val2017'
+        self.annFile='C:/Users/pasan/Documents/Motivation/cocoapi-master/PythonAPI/annotations/instances_train2017.json'.format(self.dataDir,self.dataType)
+        # initialize COCO api for instance annotations
+        self.coco=COCO(self.annFile)
+
+        # initialize COCO api for caption annotations
+        self.annfile = 'C:/Users/pasan/Documents/Motivation/cocoapi-master/PythonAPI/annotations/captions_train2017.json'.format(self.dataDir,self.dataType)
+        self.coco_caps=COCO(self.annfile)
+        # load and display caption annotations
+        self.catIds = self.coco.getCatIds(catNms=['person']);
+        temp = self.coco.getImgIds(catIds=self.catIds );
+        self.imgIds = []
+        for i in range(0, 10):
+            self.imgIds.append(temp[i])
+
+    def call(self):
+
+        print('No. of images, category : people - ', len(self.imgIds))
+        # j = 0
+        ffann = []
+        imgMap = []
+        for imgId in self.imgIds:
+            # if(j==2):
+            #     break     #To avoid loading all the annotations,  just for testing purposes
+            fann = []
+            i = 0
+            img = self.coco.loadImgs(imgId)
+            img = self.coco.loadImgs(self.imgIds[np.random.randint(0,len(self.imgIds))])[0]
+            I = io.imread('C:/Users/pasan/Documents/Motivation/cocoapi-master/PythonAPI/train2017/'+img['file_name'])#img['coco_url']
+            K = torch.from_numpy(I)
+            imgMap.append({imgId : img['coco_url']})
+            # print('\n\nImage Id: ', imgId, '\n')
+            # print( 'image tensor shape: ', K.shape)
+            annIds = self.coco_caps.getAnnIds(imgId)
+            # print(type(annIds))
+            for annId in annIds :
+                if(i< 3):
+                    fann.append(annId)
+                    i = i+1
+            # print('Annotation IDs: ')
+            # print(fann)
+            labels = self.getLabel(fann)
+            ffann.append(labels)
+            # anns = self.coco_caps.loadAnns(fann)
+            # # print('\n 3 top selected Annotations: ')
+            # anns = self.coco_caps.showAnns(anns)
+            # anns = str(anns)
+            # print('\nType of anotations', type(anns))
+            # j = j+1
+        return imgMap, ffann
+
+
+    def getimage(self, id):
+        img = self.coco.loadImgs(id)
+        img = self.coco.loadImgs(self.imgIds[np.random.randint(0,len(self.imgIds))])[0]
+        I = io.imread(img['coco_url'])
+        im = Image.fromarray(I)
+        X = preprocess.preprocess_img(im)
+        print('type of I', type(I),'\n Shape of X :', X.shape)
+        return X
+
+    def getLabel(self, ann):
+        label = []
+        anns = self.coco_caps.loadAnns(ann)
+        for i in range(0, 3):
+            label.append(anns[i]['caption'])
+        return label
+
+
+def get_verbs(caption):
+    nltk.download('wordnet')
+    nltk.download('averaged_perceptron_tagger')
+    tokens = word_tokenize(caption)
+    tags = pos_tag(tokens)
+    
+    words,tag = zip(*tags)
+    words = list(words)
+    tag =list(tag)
+    verbs_list = []
+    
+    for index,tagg in enumerate(tag):
+        if tagg=='VB'or tagg=='VBD'or tagg=='VBG'or tagg=='VBN'or tagg=='VBP'or tagg=='VBZ':
+            verbs_list.append(words[index])
+    
+    lemmatizer = WordNetLemmatizer()  
+    lemmatized_list=[]
+    for verb in verbs_list:
+        lemmatized_list.append(lemmatizer.lemmatize(verb))
+
+    return lemmatized_list
+
+
+
+
+
+
+# if __name__ == '__main__':
+#
+#     L,S = call()
+#     print ('yefbkj',len(L))
+#     print (S)
